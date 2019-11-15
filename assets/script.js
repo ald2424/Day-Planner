fc8aa375c16569d0fa1f1ca5ac10460236c09855
ald2424 <abigaild2424@gmail.com>
2019-11-15 16:37:31 -0500

    Updating code before trying to tackle button error

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,6 @@
 // *****************TODOS*********************
-// make save buttons
-// make save button work to save user input to local storage <--- Might have to save this for last
-// color code each table row depending on if it is past, present, or future
-// fix for loops that create table rows and data to make it less code
+// Color code
+// Fix buttons to only affect their sibling input field
 
 // Current day and time
 var NowMoment = moment();
@@ -15,53 +13,62 @@ var eDisplayMoment = document.getElementById('displayMoment');
  var h = NowMoment.format('h');
 
   // ***********************Set up table data*******************
+  var hours;
+  var userInput;
+  var save;
+var index = 1;
+var times = ["9:00am", "10:00am","11:00am", "12:00pm","1:00pm","2:00pm", "3:00pm", "4:00pm", "5:00pm"];
 
-  // // 9am to 11 am
-    for(var i = 9; i < 12; i++){
-      var tRow = $("<tr>");
-      var hours = $("<td>").text(i + ":00 am");
-      var userInput = $("<td>").append("<input />");
-      var save = $("<td>").append("<button />");
+// Creates table rows for each hour
+for(var i = 0; i < times.length; i++){
+  var tRow = $("<tr class='tRowClass'>");
+  hours = $("<td>").text(times[i]);
+  userInput = $("<td>").append("<input class='userInput'>");
+  save = $("<td>").append("<button class='btnClass'>" + "Save" + "</>");
 
-      tRow.append(hours, userInput, save);
-      $(".table").append(tRow);
-
-      if(i == h){
-        hours.css("color", "red");
-        }
-    }
-
-    // 12 pm
-    var tRow = $("<tr>");
-    var hours = $("<td>").text("12:00 pm");
-    var userInput = $("<td>").append("<input />");
-    var save = $("<td>").append("<button />");
-
-    tRow.append(hours, userInput, save);
+  tRow.append(hours, userInput, save);
     $(".table").append(tRow);
 
-    if(i == h){
-      tRow.css("color", "red");
+    
+
+// Checks if the hour is one digit. If so, if the hour is current the font changes
+    if(times[i].charAt(1) == ":"){
+    
+      if (times[i].charAt(0) == h){ 
+         tRow.addClass("present");         
+      }
+    }
+  // else if the number is two digits, this code pulls the first two digits (first to chars) and changes the font of what matches the current time
+    else{
+      var x = times[i].slice(0,2);
+       if(x == h){
+          tRow.addClass("present");          
+        }
+      }
+    
+  }
+  renderSavedData();
+// saves and returns user input as local storage
+  function renderSavedData(){
+      var input = localStorage.getItem("x");
+
+      if (input === null) {
+        return;
       }
 
-    // 1pm to 5pm
-    for(var i = 1; i < 6; i++){
-      var tRow = $("<tr>");
-      var hours = $("<td>").text(i + ":00 pm");
-      var userInput = $("<td>").append("<input />");
-      var save = $("<td>").append("<button />");
+      $(".userInput").replaceWith("<div>" + input + "</div>");
 
-      tRow.append(hours, userInput, save);
-      $(".table").append(tRow);
+     
+  }
 
-      if(i == h){
-        tRow.css("color", "red");
-        }
-
-    }
-
-
-  
+  $(".btnClass").on("click", function(event){
+      event.preventDefault();
+      var x = $(".userInput").val();
+      localStorage.setItem("x", x);
+      
+       renderSavedData();
+      
+  })
 
    
 
